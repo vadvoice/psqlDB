0365fe41d3151b78c7ee71e96b0c9ce3822b1f52
vadvoice <eziosheva0000@gmail.com>
2018-03-15 19:56:24 +0200

    user authentication with jwt tocken

---
+++ config/index.js
@@ -0,0 +1,25 @@
+import Joi from 'joi';
+
+// get env params
+require('dotenv').config();
+
+const envVarsSchema = Joi.object({
+    JWT_SECRET: Joi.string().required()
+      .description('JWT Secret required to sign'),
+  }).unknown()
+    .required();
+
+console.log('zzzzz: ', process.env.JWT_SECRET);
+
+const { error, value: envVars } = Joi.validate(process.env, envVarsSchema);
+
+if(error) {
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+const config = {
+    jwtSecret: envVars.JWT_SECRET,
+  };
+  
+export default config;
+  
--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,6 +1,40 @@
 
+import Joi from 'joi'
+import jwt from 'jsonwebtoken'
+import knex from '../config'
+import { isValidPass } from '../../helpers/hash.halper'
+import config from '../../config';
+
+const schema = Joi.object().keys({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+})
+
 function auth(req, res, next) {
-  res.end('fuck you!')
+  // console.log(req.body);
+  // res.end('this is the end')
+  knex('users').where({
+    email: req.body.email,
+  }).first().then(user => {
+    if(!user) throw new Error('something went wrong...')
+    if(isValidPass(user.password, req.body.password)) {
+      console.log('hoho');
+      const token = jwt.sign({
+        id: user.id,
+        email: user.email,
+      }, config.jwtSecret)
+
+      // done
+      return res.json({
+        token,
+        id: user.id,
+        email: user.email,
+      })
+    }
+  }).catch(err => {
+    console.error(err);
+    res.end('fuck you!')
+  })
 }
 
 export default auth;

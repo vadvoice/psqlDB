eab86c96e4c0d70c4b4a0f595d976e35aad87a76
vadvoice <eziosheva0000@gmail.com>
2018-03-17 22:07:11 +0200

    moved to todos list to separate place

--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,11 @@ import express from 'express'
 import authPath from './auth'
 import companiesPath from './companies'
 import usersPath from './users'
+import todosPath from './todos'
 import fs from 'fs';
 import knex from '../db/knex';
 
-const pg = require('pg');
-const path = require('path');
-const os = require('os')
-const { Pool, Client } = require('pg')
-
 const router = express.Router();
-
-router.use('/auth', authPath)
-router.use('/companies', companiesPath)
-router.use('/users', usersPath)
-
-// user info
-const user = os.userInfo().username
-const password = 'code'
-
-const connectionString = process.env.DATABASE_URL || `postgres://${user}:${password}@localhost:5432/todo`;
-
-const client = new Client({
-  connectionString: connectionString,
-})
-client.connect()
-
-const pool = new Pool({
-  connectionString: connectionString,
-})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -38,66 +15,10 @@ router.get('/', function(req, res, next) {
   })
 });
 
-router.get('/todos', (req, res, next) => {
-  const text = 'SELECT * FROM items ORDER BY id ASC;'
-  
-  // show table content
-  pool.connect((err, client, done) => {
-    if (err) throw err
-    client.query('SELECT * FROM items ORDER BY id ASC;', (err, result) => {
-      if (err) {
-        throw err.stack
-      } else {
-        console.log(result.rows)
-        if(result.rows[0] && result.rows.length) {
-          res.render('list', { data: result.rows })
-        }
-        else return res.end("You haven't got any item yet")
-      }
-    })
-  })
-
-});
-
-router.post('/todos', (req, res, next) => {
-    // Grab data from http request
-    const data = {text: req.body.text, complete: req.body.complete};
-
-    const text = 'INSERT INTO items(text, complete) VALUES($1, $2) RETURNING *'
-    const values = [data.text, data.complete]
-
-    client.query(text, values)
-      .then(res => {
-        console.log('succes:', res.rows[0])
-      })
-      .catch(e => console.error(e.stack))
-  });
-
-router.put('/todos/:todo_id', (req, res, next) => {
-  // Grab data from http request
-  const data = {text: req.body.text, complete: req.body.complete, id: req.params.todo_id};
-
-  const text = 'UPDATE items SET text=($1), complete=($2) WHERE id=($3)'
-  const values = [data.text, data.complete, data.id]
-  
-  // check selection
-  client.query('SELECT count(*) FROM items WHERE ID=($1)', [values.id]).then(r => {
-    if (r.rowCount) update()
-    else res.json({
-      message: "You haven't any deal we this id"
-    })
-  })
-
-  // UPDATE IF ITEM EXIST
-  function update() {
-    client.query(text, values)
-      .then(r => {
-        console.log('succes update:', r)
-        res.json({message: 'Vsjo'})
-      })
-      .catch(e => console.error(e.stack))
-  }
-})
+router.use('/auth', authPath)
+router.use('/companies', companiesPath)
+router.use('/users', usersPath)
+router.use('/todos', todosPath)
 
 module.exports = router;
   

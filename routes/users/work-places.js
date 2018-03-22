4870c227acd54c26fdef2f0ce3452b02fbf3bb61
vadvoice <eziosheva0000@gmail.com>
2018-03-22 16:15:42 +0200

    of cource I have to delete logs

--- routes/users/work-places.js
+++ routes/users/work-places.js
@@ -7,8 +7,7 @@ function workPlaces(req, res, next) {
   knex('users').where('id', req.query.id).first().then(resp => {
     if(!resp) return next()
     let q = knex.raw('select * from companies where ?=any(employers);', [resp.id]).then(r => {
-      console.log('::::', r.rows);
-    res.json(r.rows)
+      res.json(r.rows)
     })
   })
 }
